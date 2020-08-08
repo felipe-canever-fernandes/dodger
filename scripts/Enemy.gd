99f72f01fe504dc69d4f846bf2c81086c09fe8c2
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-07 22:25:11 -0300

    Make asteroid rotate

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,3 +1,11 @@
 tool
 extends Spawnable
 class_name Enemy
+
+var rotation_speed: float setget, get_rotation_speed
+
+func get_rotation_speed() -> float:
+	return self.speed / 100
+
+func _physics_process(delta: float) -> void:
+	rotate(self.rotation_speed * delta)
