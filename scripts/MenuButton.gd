392836afeefba7d1e59ba3942d23dd6f192fc04c
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-11 20:55:47 -0300

    Add sound for when clicking on the "New Game" button in main menu

--- scripts/MenuButton.gd
+++ scripts/MenuButton.gd
@@ -2,9 +2,14 @@ extends Button
 class_name GameButton
 
 export(AudioStream) var hover_sound: AudioStream
+export(AudioStream) var click_sound: AudioStream
 
 onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_mouse_entered():
 	audio_stream_player.stream = hover_sound
+	audio_stream_player.play()
+
+func _on_pressed():
+	audio_stream_player.stream = click_sound
 	audio_stream_player.play()
