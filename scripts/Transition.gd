2e34e043477799c3f4b684dfc2c26f038dbeca27
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-11 21:29:17 -0300

    Disable buttons when a transitions occurs

--- scripts/Transition.gd
+++ scripts/Transition.gd
@@ -6,6 +6,8 @@ const FINAL_MODULATE := Color(1, 1, 1, 0)
 var container: Container = null
 
 func play() -> void:
+	get_tree().call_group("Buttons", "set_disabled", true)
+	
 	# warning-ignore:return_value_discarded
 	interpolate_property(container, "modulate",
 		null, FINAL_MODULATE, 0.5)
