cf3bbc4488c15517bcd8f5e57bd701ac5a2c368b
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 20:55:11 -0300

    Make pause menu load screens from global script

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -1,7 +1,5 @@
 extends Control
 
-export(PackedScene) var main_menu_scene: PackedScene
-
 onready var items: Container = $Container/Items
 onready var confirmation_container: Container = $Container/ConfirmationContainer
 
@@ -25,7 +23,7 @@ func _on_MainMenuButton_pressed() -> void:
 
 func _on_YesButton_pressed() -> void:
 	# warning-ignore:return_value_discarded
-	get_tree().change_scene_to(main_menu_scene)
+	get_tree().change_scene_to(Screens.MAIN_MENU_SCENE)
 
 func _on_NoButton_pressed() -> void:
 	hide_confirmation()
