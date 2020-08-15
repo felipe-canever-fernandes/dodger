c36311888198a4aeab9722bd92255724a37b27d0
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 20:48:14 -0300

    Create global script for preloading main screens

---
+++ scripts/Screens.gd
@@ -0,0 +1,5 @@
+extends Node
+
+const NEW_GAME_MENU_SCENE := preload("res://menus/NewGameMenu.tscn")
+const LEVEL_SCENE := preload("res://levels/Main.tscn")
+const MAIN_MENU_SCENE := preload("res://menus/MainMenu.tscn")
