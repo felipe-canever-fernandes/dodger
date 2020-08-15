81c8a54350b2f8e62cab20033c049df3e3d92949
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 20:16:39 -0300

    Implement game over screen's new game button

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -1,9 +1,18 @@
 class_name GameOverScreen
 extends CenterContainer
 
+const NEW_GAME_MENU_SCENE := preload("res://menus/NewGameMenu.tscn")
+const LEVEL_SCENE := preload("res://levels/Main.tscn")
+
 onready var high_score_label: Label = $VBoxContainer/HighScoreLabel
 onready var high_level_label: Label = $VBoxContainer/HighLevelLabel
 
 func _ready() -> void:
 	high_score_label.text = "High Score: %s" % Global.high_score
 	high_level_label.text = "High Level: %s" % Global.high_level
+
+func _on_NewGameButton_pressed() -> void:
+	var scene := LEVEL_SCENE if Global.high_level == 1 else NEW_GAME_MENU_SCENE
+	
+	# warning-ignore:return_value_discarded
+	get_tree().change_scene_to(scene)
