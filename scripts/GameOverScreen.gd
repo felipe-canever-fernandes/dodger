9e0fecb220d709fa6bf111c4f0ccbaf7d0ca5308
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 20:18:44 -0300

    Add underscore in front of private variables in GameOverScreen

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -1,18 +1,20 @@
 class_name GameOverScreen
 extends CenterContainer
 
-const NEW_GAME_MENU_SCENE := preload("res://menus/NewGameMenu.tscn")
-const LEVEL_SCENE := preload("res://levels/Main.tscn")
+const _NEW_GAME_MENU_SCENE := preload("res://menus/NewGameMenu.tscn")
+const _LEVEL_SCENE := preload("res://levels/Main.tscn")
 
-onready var high_score_label: Label = $VBoxContainer/HighScoreLabel
-onready var high_level_label: Label = $VBoxContainer/HighLevelLabel
+onready var _high_score_label: Label = $VBoxContainer/HighScoreLabel
+onready var _high_level_label: Label = $VBoxContainer/HighLevelLabel
 
 func _ready() -> void:
-	high_score_label.text = "High Score: %s" % Global.high_score
-	high_level_label.text = "High Level: %s" % Global.high_level
+	_high_score_label.text = "High Score: %s" % Global.high_score
+	_high_level_label.text = "High Level: %s" % Global.high_level
 
 func _on_NewGameButton_pressed() -> void:
-	var scene := LEVEL_SCENE if Global.high_level == 1 else NEW_GAME_MENU_SCENE
+	var scene := _LEVEL_SCENE \
+			if Global.high_level == 1 \
+			else _NEW_GAME_MENU_SCENE
 	
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene_to(scene)
