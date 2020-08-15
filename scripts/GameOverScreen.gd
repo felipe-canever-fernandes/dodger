fe8ccae4ef8911fb1c98c441f28804ff50849bd4
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 20:05:28 -0300

    Display high level on game over screen

--- scripts/GameOverScreen.gd
+++ scripts/GameOverScreen.gd
@@ -2,6 +2,8 @@ class_name GameOverScreen
 extends CenterContainer
 
 onready var high_score_label: Label = $VBoxContainer/HighScoreLabel
+onready var high_level_label: Label = $VBoxContainer/HighLevelLabel
 
 func _ready() -> void:
 	high_score_label.text = "High Score: %s" % Global.high_score
+	high_level_label.text = "High Level: %s" % Global.high_level
