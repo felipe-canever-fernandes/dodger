802dd3d28b1e8c82cea33876fcc3c2951ffbef6e
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 21:51:24 -0300

    Add about screen to screens script

--- scripts/Screens.gd
+++ scripts/Screens.gd
@@ -3,3 +3,4 @@ extends Node
 const NEW_GAME_MENU_SCENE := preload("res://menus/NewGameMenu.tscn")
 const LEVEL_SCENE := preload("res://levels/Main.tscn")
 const MAIN_MENU_SCENE := preload("res://menus/MainMenu.tscn")
+const ABOUT_SCREEN_SCENE := preload("res://menus/AboutScreen.tscn")
