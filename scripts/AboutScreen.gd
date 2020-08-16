8e04fdbcf434ebe70990db3d6911dd5435fdf772
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2020-08-15 22:04:35 -0300

    Add transition to "Main Menu" button in about screen

--- scripts/AboutScreen.gd
+++ scripts/AboutScreen.gd
@@ -1,6 +1,14 @@
 class_name AboutScreen
 extends Control
 
+onready var _transition: Transition = $Transition
+onready var _container: Container = $CenterContainer
+
+func _ready() -> void:
+	_transition.container = _container
+
 func _on_MainMenuButton_pressed() -> void:
+	yield(_transition.play(), "completed")
+	
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene_to(Screens.MAIN_MENU_SCENE)
